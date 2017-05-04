75482c3e5ccd1f189fc46740f6252ce0d469b2a8
rose4288 <roskaboska88@gmail.com>
2017-05-04 10:37:57 -0400

    lists are basically working

--- codegen.ml
+++ codegen.ml
@@ -16,20 +16,23 @@ let translate (functions) =
   and i1_t  = L.i1_type  context
   and str_t = L.pointer_type (L.i8_type context)
   and float_t = L.float_type context
-  and void_t= L.void_type context in
+  and void_t= L.void_type context
+  in
 
-  let ltype_of_typ = function
+  let rec ltype_of_typ = function
       A.TInt -> i32_t
     | A.TChar -> i8_t
     | A.TBool -> i1_t
     | A.TVoid -> void_t
     | A.TString -> str_t 
-    | A.TFloat -> float_t in 
+    | A.TFloat -> float_t
+    | A.TList t -> L.pointer_type (ltype_of_typ t) in 
 
 
   (* Declare printf(), which the print built-in function will call *)
   let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func = L.declare_function "printf" printf_t the_module in
+
 
   (* Define each function (arguments and return type) so we can call it *) (** Fix the type thing here **)
   let function_decls =
@@ -94,14 +97,37 @@ let translate (functions) =
        | _ -> raise (Failure "wrong operation applied to floats")
       )
     in
+
+    let get_list_type t = (*quick utility function to map TList to the list's type*)
+    (match t with
+      A.TList x -> x
+    |  _ -> raise(Failure "problem typing lists"))
+  in
+
+  let rec assign_array ar els n builder = (*stores elements, starting with element n in ar, returns ar*)
+    match els with
+      [] -> ar
+    | e::tl -> let p = L.build_in_bounds_gep ar [|(L.const_int i32_t n)|] "ptr" builder in
+                      ignore(L.build_store e p builder); assign_array ar tl (n+1) builder
+    in
+
+
     let rec aexpr builder local_var_map = function
         A.AIntLit(i, _) -> L.const_int i32_t i
       | A.ABoolLit(b, _) -> L.const_int i1_t (if b then 1 else 0)
       | A.AStrLit(s, _) -> L.build_global_stringptr s "str" builder
-      (*| A.ACharLit(c, _) -> L.const_int i8_t c*)
+      | A.ACharLit(c, _) -> L.const_int i8_t (C.code c)
       | A.AFloatLit(f, _) -> L.const_float float_t f
       | A.AId(s,_) -> L.build_load (lookup s local_var_map) s builder 
-      (* | A.List ->  why is List an expression, should not it be a data staructure?  *)
+      | A.AList(l, t) ->  let list_typ = get_list_type t and els = List.map (aexpr builder local_var_map) l in 
+        (*let init = (L.const_array (ltype_of_typ list_typ) (Array.of_list els)) in 
+        let glob = (*L.define_global "lst" init the_module*) in*)
+        let ar_var = L.build_array_alloca (ltype_of_typ list_typ) (L.const_int i32_t (List.length l)) "lst" builder in
+        assign_array ar_var els 0 builder
+        (*ignore (L.build_store (L.const_array (ltype_of_typ list_typ) (Array.of_list els)) ar_var builder); *)
+        (*L.const_bitcast ar_var (L.pointer_type (ltype_of_typ list_typ))*)
+      | A.AItem(s, e, t) -> let ar = L.build_load (lookup s local_var_map) "ar" builder and ad = aexpr builder local_var_map e in
+                            let p = L.build_in_bounds_gep ar [|ad|] "ptr" builder in L.build_load p "item" builder 
       | A.ACall ("print", [e], _, _) -> L.build_call printf_func [| (aexpr builder local_var_map e) |] "printf" builder
       | A.ACall (f, act, _, _) ->
         let (fdef, fdecl) = StringMap.find f function_decls in
@@ -109,7 +135,6 @@ let translate (functions) =
         let result = (match fdecl.A.typ with A.TVoid -> ""
                                            | _ -> f ^ "_result") in
         L.build_call fdef (Array.of_list actuals) result builder
-      (* | A.List ->  why is List an expression, should not it be a data staructure?  *)
       | A.AUnop(op, e, t) ->
             let e' = aexpr builder local_var_map e in
            (match op with
@@ -144,9 +169,11 @@ let translate (functions) =
             let local_var = L.build_alloca (ltype_of_typ t) n builder
             in StringMap.add n local_var m in
           (match s with
-          AId(name, typ) -> let local_var_map = add_local local_var_map (t,name) in 
+          A.AId(name, typ) -> let local_var_map = add_local local_var_map (t,name) in 
            let e' = aexpr builder local_var_map e in ignore (L.build_store e' (lookup name local_var_map) builder); (builder, local_var_map)
-          | _ -> (*do something to store an item in a list*) (builder, local_var_map))
+          | A.AItem(name, adr, typ) -> let e' = aexpr builder local_var_map e and 
+            ar = L.build_load (lookup name local_var_map) "ar" builder and ad = aexpr builder local_var_map adr in
+            let p = L.build_in_bounds_gep ar [|ad|] "ptr" builder in ignore(L.build_store e' p builder); (builder, local_var_map))
         | A.AIf (predicate, then_stmt, else_stmt) ->
           let bool_val = aexpr builder local_var_map predicate in
           let merge_bb = L.append_block context "merge" the_function in
