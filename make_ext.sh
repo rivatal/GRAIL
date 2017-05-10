edd10d063225e1a55b0192caec6ca3ddf97ce390
Aashima <aashima.arora@columbia.edu>
2017-05-10 18:20:08 -0400

    FINAL Makefile

--- make_ext.sh
+++ make_ext.sh
@@ -1,7 +1,12 @@
-
-./grail.native < pete.gl > out.ll && 
+if [ -n "$1" ]; then 
+   FILE="$1"
+else
+   echo -n "File name is a required argument. Enter a .gl file. "
+   exit
+fi
+./grail.native <"$FILE" > out.ll && 
 clang -o final out.ll ./external/disp.c -lm
 
-mv final bin
+mv final bin/
 cd bin
 ./final
